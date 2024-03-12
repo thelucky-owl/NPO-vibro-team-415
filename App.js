a0168153bea652386a43610d56cd463e336f19ce
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2024-03-12 17:17:54 +0100

    Made it!

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Button, Text, Vibration} from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  const [recording, setRecording] = useState();
+  const [permissionResponse, requestPermission] = Audio.usePermissions();
+  const [soundLevel, setSoundLevel] = useState(null);
+
+  async function startRecording() {
+    try {
+      if (permissionResponse.status !== 'granted') {
+        console.log('Requesting permission..');
+        await requestPermission();
+      }
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HighQuality
+      );
+      setRecording(recording);
+
+      recording.setOnRecordingStatusUpdate((status) => {
+        if (status.isRecording) {
+          // console.log(status)
+          setSoundLevel(status.metering + 100);
+           if ((status.metering + 100) > 30) {
+            Vibration.vibrate((Math.floor((status.metering) + 100) / 10) * 80)
+           };
+        }
+      });
+
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    await recording.stopAndUnloadAsync()
+    setRecording(undefined);
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.volumeText}>
+        {soundLevel !== null ? `Sound Level: ${soundLevel}` : 'Waiting for sound level data...'}
+      </Text>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording}
+      />
     </View>
   );
 }
@@ -13,8 +65,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
+  },
+  volumeText: {
+    marginBottom: 10,
+    textAlign: 'center',
   },
 });
