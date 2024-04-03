b9853b7de792c0261d2fdddb66d9c92e127f6c26
1028473 - Mees Muller <89028475+TheMazeIsAmazing@users.noreply.github.com>
2024-04-03 22:03:31 +0200

    Fixes

--- HomeScreen.js
+++ HomeScreen.js
@@ -8,13 +8,11 @@ const HomeScreen = ({ navigation, inputTimes }) => {
     return (
         <View style={styles.container}>
             <Text>
-                Welkom demo van NPO Vibro van Team 415. We bieden twee verschillende demo's aan: één met een videospeler en één zonder. De demo met de videospeler maakt het mogelijk om met één druk op de knop zowel de video als de 'vibro track' te starten. Echter, deze ervaring is niet optimaal omdat veel trillingen ontbreken. De andere demo bevat geen video, maar biedt een meer gedetailleerd vibratiepatroon. Als je dit patroon wilt synchroniseren met de muziekvideo van Europapa, moet je de app en de video tegelijkertijd starten.
-            </Text>
+                Welkom bij de demo van NPO Vibro, ontwikkeld door Team 415. We bieden twee verschillende demo's aan: één met een videospeler en één zonder. De demo met de videospeler maakt het mogelijk om met één druk op de knop zowel de video als het 'vibro track' te starten. Echter, deze ervaring is niet optimaal omdat veel trillingen ontbreken. De andere demo bevat geen video, maar biedt een meer gedetailleerd vibratiepatroon. Als je dit patroon wilt synchroniseren met de muziekvideo van Europapa, moet je de app en de video tegelijkertijd starten.            </Text>
             <Text>
             </Text>
             <Text>
-                Het vibratiepatroon eindigt rond 1:25, het klopt dus dat die midden in het nummer stopt met trillen.                                                                                                                                                      .
-            </Text>
+                Het vibratiepatroon eindigt rond 1:25, dus het klopt dat de trillingen midden in het nummer stoppen            </Text>
             <Text>
 
             </Text>
